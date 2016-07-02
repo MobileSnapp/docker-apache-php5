005385db783d162e4c81baf689eb7c3ca2790035
= <sudeep@mobilesnapp.com>
2016-07-02 01:07:07 -0700

    Adding script and setup configurations

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Apache-PHP7 Docker Container</title>
+</head>
+<body>
+<h1>Welcome to docker container for Apache-PHP5</h1>
+<h3>You are successfully running Apache v2.* and PHP 5.* in a docker container.</h3>
+<?php echo "Hello from PHP. PHP running successfully"; ?>
+<p>Docker image provided by MobileSnapp Inc. (support@mobilesnapp.com)</p>
+</body>
+</html>
+Status API Training Shop Blog About
